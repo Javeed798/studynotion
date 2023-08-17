ccaf93e5a9ca0a8516cd8b4ac6fce9c7b5289f3f
D Javeed Sharif <karishmavahida@gmail.com>
2023-08-17 17:22:13 +0530

    home Page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import {Route, Routes} from "react-router-dom";
-import Home from "./pages/Home";
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className={"w-screen min-h-screen bg-richblack-900 flex flex-col font-inter"}>
-      <Routes>
-          <Route path="/" element={<Home />} />
+    <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
+    <Routes>
+      <Route path="/" element={<Home />} />
       </Routes>
-    </div>
+      </div>
   );
 }
 
