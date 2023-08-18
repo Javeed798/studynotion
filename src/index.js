08b3c800bdf97d61f4c129a2d677a66bede8c181
D Javeed Sharif <karishmavahida@gmail.com>
2023-08-18 09:41:41 +0530

    Creation of slices reducers and configuring store

--- src/index.js
+++ src/index.js
@@ -3,10 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import rootReducer from './reducer';
+import { Toaster } from 'react-hot-toast';
+
+const store = configureStore({
+    reducer : rootReducer
+})
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
+     <Provider store={store}>
      <App />
+      <Toaster />
+     </Provider>
     </BrowserRouter>
     
 );
