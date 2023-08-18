20dbf7386f4f2607f0ffdac24d307a5160d6d0db
D Javeed Sharif <karishmavahida@gmail.com>
2023-08-18 14:40:03 +0530

    combining slices into one reducer for creation of store

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -3,6 +3,7 @@ import authSlice from "../slices/authSlice"
 import profileSlice from "../slices/profileSlice";
 import cartSlice from "../slices/cartSlice";
 
+//  instead of using configureStore we are using this combine reducer 
 const rootReducer = combineReducers({
   auth : authSlice,
   profile : profileSlice,
