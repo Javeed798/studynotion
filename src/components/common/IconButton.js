773721ec45d2e7f4605152fd0645bf722bb49fc9
D Javeed Sharif <karishmavahida@gmail.com>
2023-08-21 17:58:24 +0530

    Contact Form DOne

---
+++ src/components/common/IconButton.js
@@ -0,0 +1,19 @@
+const IconButton = ({text,onclick, children, disabled, outline=false, customClasses, type}) => {
+  return (
+    <button
+      disabled={disabled}
+      onClick={onclick}
+      type= {type}
+      >{children 
+      ? (
+        <>
+          <span>{text}</span>
+          {children}
+        </>
+      ) 
+      : (text)
+      }
+    </button>
+  )
+}
+export default IconButton
--- src/components/core/AboutPage/ContactFormSection.js
+++ src/components/core/AboutPage/ContactFormSection.js
@@ -1,15 +1,18 @@
-import ContactUsForm from "../../ContactUsPage/ContactUsForm"
+import React from "react";
+import ContactUsForm from "../../ContactPage/ContactUsForm";
 
 const ContactFormSection = () => {
   return (
     <div className="mx-auto">
-      <h1>Get In Touch</h1>
-      <p>We'd love to help u. Please Fill The Form</p>
-
-      <div>
+      <h1 className="text-center text-4xl font-semibold">Get in Touch</h1>
+      <p className="text-center text-richblack-300 mt-3">
+        We&apos;d love to here for you, Please fill out this form.
+      </p>
+      <div className="mt-12 mx-auto">
         <ContactUsForm />
       </div>
     </div>
-  )
-}
-export default ContactFormSection
+  );
+};
+
+export default ContactFormSection;
